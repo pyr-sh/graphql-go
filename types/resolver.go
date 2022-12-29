d1c7d388fac42f873271192ed8fe8b803e1ef274
Max Twardowski <twardowskimax@gmail.com>
2022-12-29 18:01:23 +0100

    GetFieldNames v1

--- types/resolver.go
+++ types/resolver.go
@@ -6,39 +6,55 @@ import (
 	"github.com/pkg/errors"
 )
 
+// ResolverContext is a custom context structure w/ additional data meant to be used in resolver methods
 type ResolverContext struct {
 	context.Context
-	Operation  *OperationDefinition
-	Definition *ExecutableDefinition
 
-	// Internal
+	op             *OperationDefinition
+	def            *ExecutableDefinition
 	fragmentByName map[string]*FragmentDefinition
 }
 
 func NewResolverContext(ctx context.Context, o *OperationDefinition, d *ExecutableDefinition) ResolverContext {
 	rc := ResolverContext{ctx, o, d, map[string]*FragmentDefinition{}}
-	for _, f := range d.Fragments {
+	for _, f := range d.Fragments { // construct the fragments mapping
 		rc.fragmentByName[f.Name.Name] = f
 	}
 	return rc
 }
 
-func (s ResolverContext) FindRoot(path ...string) Selection {
-	return s.find(s.Operation.Selections, path...)
+func (rc ResolverContext) GetFieldNames(path ...string) (res []string) {
+	found := rc.findField(rc.op.Selections, path...)
+	for _, sel := range found.SelectionSet {
+		res = append(res, rc.fieldNames(sel)...)
+	}
+	return
 }
 
-func (s ResolverContext) find(set SelectionSet, path ...string) Selection {
-	var found Selection
+func (rc ResolverContext) fieldNames(sel Selection) (res []string) {
+	switch sel := sel.(type) {
+	case *Field:
+		res = append(res, sel.Name.Name)
+	case *InlineFragment, *FragmentSpread:
+		for _, s := range rc.getSelections(sel) {
+			res = append(res, rc.fieldNames(s)...)
+		}
+	default:
+		panic(errors.Errorf("unhandled type %T", sel))
+	}
+	return
+}
+
+func (rc ResolverContext) findField(set SelectionSet, path ...string) *Field {
+	var found *Field
 	for _, sel := range set {
 		switch sel := sel.(type) {
 		case *Field:
 			if sel.Name.Name == path[0] {
 				found = sel
 			}
-		case *InlineFragment:
-			found = s.find(sel.Fragment.Selections, path...)
-		case *FragmentSpread:
-			found = s.find(s.getSelections(sel), path...)
+		case *InlineFragment, *FragmentSpread:
+			found = rc.findField(rc.getSelections(sel), path...)
 		default:
 			panic(errors.Errorf("unhandled type %T", sel))
 		}
@@ -51,17 +67,17 @@ func (s ResolverContext) find(set SelectionSet, path ...string) Selection {
 	if len(path) == 0 || found == nil {
 		return found
 	}
-	return s.find(s.getSelections(found), path...)
+	return rc.findField(rc.getSelections(found), path...)
 }
 
-func (s ResolverContext) getSelections(sel Selection) SelectionSet {
+func (rc ResolverContext) getSelections(sel Selection) SelectionSet {
 	switch sel := sel.(type) {
 	case *Field:
 		return sel.SelectionSet
 	case *InlineFragment:
 		return sel.Fragment.Selections
 	case *FragmentSpread:
-		frag, ok := s.fragmentByName[sel.Name.Name]
+		frag, ok := rc.fragmentByName[sel.Name.Name]
 		if !ok {
 			return nil
 		}
